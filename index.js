41b68c4d5bbe524802cd04e3e7afb62a8d5d99b8
quick-photography <90473484+quick-photography@users.noreply.github.com>
2021-11-08 02:37:46 +0700

    change host

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const createApp = require('./app')
 
 const app = createApp(process.env)
 
-app.listen(process.env.PORT, (err, address) => {
+app.listen(process.env.PORT, '0.0.0.0', (err, address) => {
   if (err) {
     console.log(err)
     process.exit(1)
